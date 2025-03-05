a81fc9a8aa2aa332af2d3420e9b7c12cab308673
devops3181 <yukon3181@gmail.com>
2025-03-05 03:05:13 +0300

    Your commit message

--- script.sh
+++ script.sh
@@ -1,22 +1,3 @@
 echo "Hello World"
 echo ""[Dtest creds[C[D"[B
-echo ""[1;2D[D[Cwhat am i doing here[C[C[C[D[D[D"[B[B[B[B
-
-
-
-
-
-
-
-
-[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[C[C[C[C[C[C[C[C[A[A[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-
-
-
-
-
-
-
-[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-
-git add..
+echo ""[1;2D[D[Cwhat am i doing here[C[C[C[D[D[D"[B[B[
